cac2548c5661318d91c25c12e57d0b8417f71417
lauramekaj <47980185+lauramekaj@users.noreply.github.com>
2019-05-17 14:56:11 +0300

    Add files via upload

---
+++ MicroProject Python.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+###This is the Python code
+
+
+# In[1]:
+
+
+q=[[-3,3],[5,-5]]
+
+
+# In[2]:
+
+
+t=0
+
+
+# In[3]:
+
+
+qt=q*t
+
+
+# In[4]:
+
+
+qt
+
---
+++ MicroProject R.r
@@ -0,0 +1,8 @@
+
+q<-matrix(c (-3,3,5,-5), nrow=2, ncol=2, byrow=TRUE)
+
+t<-c(0)
+
+qt<-q*t
+
+qt
